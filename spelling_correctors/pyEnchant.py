d72e85b5b13b5eccec8b23fc2bb0f49cf4b71601
Felicia Redelaar <fredelaar@gmail.com>
2022-05-12 20:54:53 +0200

    pyenchant completely working! fixed both train and spell search query

--- spelling_correctors/pyEnchant.py
+++ spelling_correctors/pyEnchant.py
@@ -1,48 +1,73 @@
 import enchant
 import re
 import pandas as pd
+import string
 from enchant.checker import SpellChecker
+import datetime
+import csv
+
+begin_time = datetime.datetime.now()
 
 d = enchant.Dict("en_US")
 
-traindata = pd.read_csv('../home-depot-data/train.csv', encoding="ISO-8859-1")
+traindata = pd.read_csv('../home-depot-data/test.csv', encoding="ISO-8859-1")
 searchQueries = traindata["search_term"]
+
 
 # The suggestions are returned in a list, ordered from most likely replacement to least likely. source:
 # https://pyenchant.github.io/pyenchant/tutorial.html
+
+def containsNumber(value):
+    for character in value:
+        if character.isdigit():
+            return True
+    return False
+
 
 def repairWordsAutomatically(wordlist):
     """
     Function that will repair words automatically. Correct words will be skipped, faulty words repaired with the best
     PyEnchant suggestion. :param wordlist:
     """
-    """" TO DO: Only lists single words are working now. We also have search queries consisting of multiple words.
-         Method: Check for each search_Term whether there is more than 1 word. If so, check the entire block of text.
-         Then, when errored words come up, repair those and put all words back in as 1 search term query."""
-
 
     wordsCheckedOrCorrected = []
     for word in wordlist:
+        word = word.translate(str.maketrans('', '', string.punctuation))
+
         if len(word.split()) > 1:
             TempStr = ''
             for x in word.split():
-                if not d.check(x):
+                if containsNumber(x):
+                    TempStr = TempStr + ' ' + x
+                elif not d.check(x):
                     sug = d.suggest(x)
-                    sug = sug[0]
-                    TempStr = TempStr + ' ' + sug
+                    try:
+                        sug = sug[0]
+                        TempStr = TempStr + ' ' + sug
+                    except:
+                        print('no better suggestion found')
                 else:
                     TempStr = TempStr + ' ' + x
             TempStr = TempStr.strip()
             wordsCheckedOrCorrected.append(TempStr)
-        else:
-            if not d.check(word):
+        else:  # search query is one word
+            if containsNumber(word):
+                wordsCheckedOrCorrected.append(word)
+            elif not d.check(word):
                 sug = d.suggest(word)
-                sug = sug[0]
-                wordsCheckedOrCorrected.append(sug)
+                try:
+                    sug = sug[0]
+                    wordsCheckedOrCorrected.append(sug)
+                except:
+                    print('no better suggestion found')
             else:
                 wordsCheckedOrCorrected.append(word)
     print(wordsCheckedOrCorrected)
+    df = pd.DataFrame(wordsCheckedOrCorrected)
+    df.to_csv('PyEnchant-test.csv', index=False, header=False)
 
+repairWordsAutomatically(searchQueries)
 
-repairWordsAutomatically(['dog', 'cat', 'cat hello omg', 'dog', 'gud', 'good dog', 'sprinkelr'])
-# repairWordsAutomatically(searchQueries)
+print(datetime.datetime.now() - begin_time)
+
+# 0:21:05.866842  21 minutes for training data to be processed.
