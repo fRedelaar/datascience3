ca2045e685a02a180612da048b2a1147c895891d
Felicia Redelaar <fredelaar@gmail.com>
2022-05-18 08:46:53 +0200

    deleted some files

--- helpers/lowercase_brands.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df_attr = pd.read_csv('brandnames.csv', encoding="ISO-8859-1")
-df_attr.columns =['brands']
-
-df_attr['brands'] = df_attr['brands'].str.lower()
-
-df_attr.to_csv('brandnames-lower.csv', index=False, header=False)
